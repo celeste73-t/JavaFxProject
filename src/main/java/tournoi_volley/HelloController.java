3b667c188b5782679602c8ccc5b32bd6d62377e1
Celeste_Tollec <celeste73310@gmail.com>
2025-05-14 09:13:53 +0200

    base

---
+++ src/main/java/tournoi_volley/HelloController.java
@@ -0,0 +1,14 @@
+package tournoi_volley;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
